3f01c0ab7e5399a9a3858e028de7db8fc588ad05
Evgeny Khatskov <45400445+YVM2L@users.noreply.github.com.>
2025-07-17 12:31:32 +0300

    test button into html ; added a new index.js

--- index.js
+++ index.js
@@ -1,78 +1,13 @@
-function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
-  const randomIndex = Math.floor(Math.random() * choices.length);
-  return choices[randomIndex];
-}
+let playerScore = 0;
+let computerScore = 0;
+let round = 1; //текущий раунд
 
-// console.log(getComputerChoice());
+//начинаю подключать html по id
+const resultMessage = document.getElementById("result");
+const winnerMessage = document.getElementById("winner-message");
+const playerScoreDisplay = document.getElementById("player-score");
+const computerScoreDisplay = document.getElementById("computer-score");
 
-function getHumanChoice() {
-  const userInput = prompt("Choose: rock, paper or scissors");
-  return userInput.toLowerCase();
-}
-
-// console.log(getHumanChoice());
-
-function playRound(player, computer) {
-  if (player === computer) {
-    return "It's a tie!";
-  }
-
-  if (
-    (player === "rock" && computer === "scissors") ||
-    (player === "scissors" && computer === "paper") ||
-    (player === "paper" && computer === "rock")
-  ) {
-    return "You win!";
-  }
-
-  return "You lose!";
-}
-
-function playGame() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 1; i <= 5; i++) {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    const result = playRound(humanChoice, computerChoice);
-
-    if (result === "You win!") {
-      playerScore++;
-    } else if (result === "You lose!") {
-      computerScore++;
-    }
-
-    document.getElementById(
-      "current-score"
-    ).textContent = `Final Score - Player: ${playerScore}, Computer : ${computerScore}`;
-    if (playerScore > computerScore) {
-      document.getElementById("winner-message").textContent =
-        "You won the Game!";
-    } else if (playerScore < computerScore) {
-      document.getElementById("winner-message").textContent =
-        "Computer won the Game!";
-    } else {
-      document.getElementById("winner-message").textContent =
-        "It's an overall tie!";
-    }
-
-    console.log("You chose:", humanChoice);
-    console.log("Computer chose:", computerChoice);
-    console.log(`Round ${i}: ${result}`);
-    // console.log(result);
-  }
-
-  console.log(
-    `Final Score - Player: ${playerScore}, Computer : ${computerScore}`
-  );
-
-  if (playerScore > computerScore) {
-    console.log("You won the game!");
-  } else if (playerScore < computerScore) {
-    console.log("Computer won the game!");
-  } else {
-    console.log("It's an overall tie!");
-  }
-}
+console.log("Round:", round);
+console.log("playerScore DOM", playerScoreDisplay);
+playerScoreDisplay.textContent = "99" //смена текста
