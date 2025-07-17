ae6979961ab4c43b80584504b5749adb5754fe45
Evgeny Khatskov <45400445+YVM2L@users.noreply.github.com.>
2025-07-17 12:49:26 +0300

    added function computerchoise

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const winnerMessage = document.getElementById("winner-message");
 const playerScoreDisplay = document.getElementById("player-score");
 const computerScoreDisplay = document.getElementById("computer-score");
 
-console.log("Round:", round);
-console.log("playerScore DOM", playerScoreDisplay);
-playerScoreDisplay.textContent = "99" //смена текста
+//console.log("Round:", round); 
+//console.log("playerScore DOM", playerScoreDisplay);
+//playerScoreDisplay.textContent = "99" //смена текста
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex]; // число от 0 до 1, умножили, округлили и сопоставили
+}
+console.log(getComputerChoice()); //проверяем что выбирает бот
